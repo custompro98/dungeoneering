51280e702b18e906a42f1eff5c777fd760bbe828
custompro98 <mitchjoa@gmail.com>
2022-08-03 15:55:54 -0400

    Move roll handling up to the roll creator, add rollable id, add form input to parse rollables

---
+++ components/die.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useState } from 'react'
+
+import { Die as Rollable } from 'roller.ts'
+
+type Props = {
+  rollable: Rollable
+  onReroll: (r: Rollable) => void
+}
+
+const Die = ({ rollable, onReroll }: Props) => {
+  const [roller, _] = useState(rollable)
+  const [result, setResult] = useState(rollable.value())
+
+  const handleClick = () => {
+    onReroll(roller)
+    setResult(roller.value())
+  }
+
+  return (
+    <div className="flex flex-col basis-1/5 justify-center items-center border h-12 w-12">
+      {result}
+      <button onClick={handleClick} className="hover:bg-gray-600 text-xs">Reroll</button>
+    </div>
+  )
+}
+
+export default Die
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,21 @@
 import type { NextPage } from 'next'
-import { useState } from 'react'
-import { Die, d2, d4, d6, d8, d10, d20, d100 } from 'roller.ts'
+import Image from 'next/image'
+import { FormEventHandler, useState } from 'react'
+import {
+  Die as Rollable,
+  d2,
+  d4,
+  d6,
+  d8,
+  d10,
+  d20,
+  d100,
+  parse,
+} from 'roller.ts'
 import type { RollFunction } from 'roller.ts'
+import { v4 as uuidv4 } from 'uuid'
+
+import Die from '../components/die'
 
 const diceMap: { [key: string]: RollFunction } = {
   2: d2,
@@ -13,34 +27,87 @@ const diceMap: { [key: string]: RollFunction } = {
   100: d100,
 }
 
+type RollableWrapper = {
+  rollable: Rollable
+  id: string
+}
+
 const Home: NextPage = () => {
-  const [dice, setDice] = useState([] as Die[])
+  const [dice, setDice] = useState([] as RollableWrapper[])
+  const [input, setInput] = useState('')
+
+  const generateWrapper = (rollable: Rollable): RollableWrapper => {
+    return {
+      id: uuidv4(),
+      rollable,
+    }
+  }
 
   const onAddDie = (die: number) => () => {
-    setDice([...dice, new Die(diceMap[die])])
+    setDice([...dice, generateWrapper(new Rollable(diceMap[die]))])
+  }
+
+  const handleReroll = (rollable: Rollable) => {
+    rollable.reroll()
+  }
+
+  const handleParse = (input: string) => {
+    setDice([])
+
+    const rollables = parse(input) as Rollable[]
+    console.log(rollables.map((r) => r.value()))
+    setDice(rollables.map(generateWrapper))
+  }
+
+  const handleChange: FormEventHandler<HTMLInputElement> = (e) => {
+    setInput((e.target as HTMLInputElement).value)
+  }
+
+  const handleFormSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault()
+
+    handleParse(input)
+    setInput('')
   }
 
   return (
-    <div className="bg-gray-800 text-slate-300 flex flex-row h-screen w-screen pb-4 pt-4">
-      <div className="flex flex-col justify-between h-full w-fit">
-        <button onClick={onAddDie(2)}>d2</button>
-        <button onClick={onAddDie(4)}>d4</button>
-        <button onClick={onAddDie(6)}>d6</button>
-        <button onClick={onAddDie(8)}>d8</button>
-        <button onClick={onAddDie(10)}>d10</button>
-        <button onClick={onAddDie(20)}>d20</button>
-        <button onClick={onAddDie(100)}>d100</button>
+    <div className="bg-gray-800 text-slate-300 flex flex-col w-screen h-full pb-4 pt-4 ">
+      <div className="flex flex-row justify-between pl-4 pr-4">
+        <Image
+          src="/vercel.svg"
+          alt="logo: placeholder"
+          width="128"
+          height="32"
+        />
+
+        <form onSubmit={handleFormSubmit} className="space-x-2">
+          <input
+            type="text"
+            value={input}
+            onChange={handleChange}
+            placeholder="Enter dice pattern..."
+            className="text-slate-700 rounded px-1"
+          />
+          <input type="submit" className="border rounded px-2" />
+        </form>
       </div>
-      <div className="flex flex-row flex-wrap w-full max-w-lg mx-auto">
-        {dice.map((die, idx) => (
-          <div
-            key={idx}
-            className="flex flex-col basis-1/5 justify-center items-center border h-12 w-12"
-          >
-            {die.value()}
-            <button className="text-xs">Reroll</button>
-          </div>
-        ))}
+      <div className="flex flex-row h-screen w-screen pb-4 pt-4">
+        <div className="flex flex-col justify-around w-fit">
+          <button onClick={onAddDie(2)}>d2</button>
+          <button onClick={onAddDie(4)}>d4</button>
+          <button onClick={onAddDie(6)}>d6</button>
+          <button onClick={onAddDie(8)}>d8</button>
+          <button onClick={onAddDie(10)}>d10</button>
+          <button onClick={onAddDie(20)}>d20</button>
+          <button onClick={onAddDie(100)}>d100</button>
+        </div>
+        <div className="flex flex-row flex-wrap w-full max-w-lg mx-auto">
+          {dice.map((die) => (
+            <div key={die.id}>
+              <Die rollable={die.rollable} onReroll={handleReroll} />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   )
