bf230150bc2725a97162b065701891b25d1dcc98
custompro98 <mitchjoa@gmail.com>
2022-10-23 14:32:06 -0400

    Center home page text properly

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex flex-col justify-center items-center border border-gray-600 rounded h-1/3 w-1/3 mt-32">
+    <div className="flex flex-col justify-center items-center text-center border border-gray-600 rounded h-1/3 w-1/3 mt-32">
       <h2 className="text-xl">let the adventure begin</h2>
     </div>
   )
